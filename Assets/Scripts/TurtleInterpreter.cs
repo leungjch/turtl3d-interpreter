a7d1970666082cb09a21ea393673ec4858545c3f
Justin <justin.leung.ch@gmail.com>
2021-01-06 11:21:04 -0500

    Redo lexer to be more specific with keywords

--- Assets/Scripts/TurtleInterpreter.cs
+++ Assets/Scripts/TurtleInterpreter.cs
@@ -9,14 +9,17 @@ public class TurtleInterpreter
 
     public List<Token> TokensList = new List<Token>(); 
 
+
     // FOR LEXER
     // Enums of possible types of tokens
     public enum TokenType 
     {
-        Number,
-        Word,
-        LParen,
-        RParen
+        PRIMITIVE,
+        IDENTIFIER, // e.g. the "fd" in fd 10, the "x" in x=1. Can be function or variable. 
+        CONSTANT,   // e.g. the "10" in fd 10, the "1" in x=1. Just numbers.
+        L_PAREN,     // [
+        R_PAREN,     // ]
+        EOF         // Always at the end of input
     }
 
     // FOR LEXER
@@ -33,6 +36,25 @@ public class TurtleInterpreter
             _literal = lit;
         }
     }
+
+
+    // FOR LEXER
+
+    // Primitives defined in the logo language
+    public enum PrimitiveType 
+    {
+        FORWARD,
+        BACKWARD,
+        LEFT,
+        RIGHT
+    }
+    // Dictionary of primitive types aka (reserved) keywords
+    public Dictionary<string, PrimitiveType> KeywordLookupTable = new Dictionary<string, PrimitiveType>
+    {
+        { "fd", PrimitiveType.FORWARD },
+        { "bk", PrimitiveType.BACKWARD }
+    };
+
 
     // FOR PARSER
     public enum ExpressionType 
@@ -73,8 +95,13 @@ public class TurtleInterpreter
                     tokenLiteral += restOfWordChar;
                     i += 1;
                 }
+
+                // TODO
+                // Add keyword word checking here
+                // For now, assume all words are primitives (fd / bk)
+
                 
-                tokensList.Add(new Token(TokenType.Word, tokenLiteral));
+                tokensList.Add(new Token(TokenType.PRIMITIVE, tokenLiteral));
             }
 
             // If first character is numeric, then it's a number.
@@ -89,7 +116,7 @@ public class TurtleInterpreter
                     i += 1;
                 }
                 
-                tokensList.Add(new Token(TokenType.Number, tokenLiteral));
+                tokensList.Add(new Token(TokenType.CONSTANT, tokenLiteral));
             }
             else 
             {
@@ -97,10 +124,13 @@ public class TurtleInterpreter
             }
         }
 
+        // Add EOF token at end
+        tokensList.Add(new Token(TokenType.EOF, "EOF"));
+
         // Debugging
         foreach (Token tok in tokensList)
         {
-            Debug.Log(tok._literal);
+            Debug.Log(tok._literal + tok._tokenType);
         }
         TokensList = tokensList; 
         return tokensList;
@@ -110,7 +140,7 @@ public class TurtleInterpreter
     // Takes TokenList and builds an Abstract Syntax Tree
     public void parser() 
     {
-        expressionList = new List<ExpressionTreeNode>();
+        expressionList = new List<AbstractSyntaxTreeNode>();
         int i = 0;
         while (i < TokensList.Count) 
         {   
