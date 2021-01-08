8fe130e25786fbc8bfed583739094fb8776e979c
Justin <justin.leung.ch@gmail.com>
2021-01-07 21:47:55 -0500

    Add turtle speed

--- Assets/Scripts/Turtle.cs
+++ Assets/Scripts/Turtle.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Turtle : MonoBehaviour
 {
     Rigidbody m_Rigidbody;
-    float m_Speed;
+    float scale;
     float progress = 0.0f;
+    int commandsPerUpdate = 100;
 
     public Queue<Command> commandQueue = new Queue<Command>();
+
+    // Keep track of position history for drawing line
+    public List<Vector3> positionHistory = new List<Vector3>();
+
+    // Line renderer object
+    LineRenderer line; 
 
     public class Command 
     {
@@ -25,84 +32,100 @@ public class Turtle : MonoBehaviour
     {
         //Fetch the Rigidbody component you attach from your GameObject
         m_Rigidbody = GetComponent<Rigidbody>();
+        line = gameObject.AddComponent<LineRenderer>();
         //Set the speed of the GameObject
-        m_Speed = 80; // value between 0 and 100
+        scale = 1; // value between 0 and 100
     }
 
     void Update()
     {
         // Debug.Log("Num commands" + commandQueue.Count);
         // Get current command
-        if (commandQueue.Count > 0)
+        for (int i = 0; i < commandsPerUpdate; i++)
         {
-            Command currentCommand = commandQueue.Peek();
-            float valueIncrement = currentCommand.val / 100.0f;
-            Debug.Log("Current command" + currentCommand.action + currentCommand.val);
-
-            switch (currentCommand.action)
+            if (commandQueue.Count > 0)
             {
-                case "fd":
-                    m_Rigidbody.position += transform.forward * valueIncrement * m_Speed;
-                    break;
-                case "bk":
-                    m_Rigidbody.position -= transform.forward * valueIncrement * m_Speed;
-                    break;
-                case "lt": 
-                    transform.Rotate(new Vector3(1, 0, 0) * currentCommand.val);
-                    progress = 100;
-                    break;
-                case "rt":
-                    transform.Rotate(new Vector3(1, 0, 0) * currentCommand.val);
-                    progress = 100;
-                    break;  
-                case "up":
-                    transform.Rotate(new Vector3(0, 1, 0) * currentCommand.val);
-                    progress = 100;
-                    break;
-                case "dn":
-                    transform.Rotate(new Vector3(0, 1, 0) * -currentCommand.val);
-                    progress = 100;
-                    break;
-            }
+                Command currentCommand = commandQueue.Peek();
+                float valueIncrement = currentCommand.val;
+                Debug.Log("Current command" + currentCommand.action + progress);
 
-            if (progress >= 100.0f)
-            {
+                Vector3 oldPos = m_Rigidbody.position;
+
+                switch (currentCommand.action)
+                {
+                    case "fd":
+                        // m_Rigidbody.position += transform.forward * valueIncrement * scale * Time.deltaTime;
+                        m_Rigidbody.position += transform.forward * valueIncrement * scale;
+
+                        break;
+                    case "bk":
+                        m_Rigidbody.position -= transform.forward * valueIncrement * scale;
+                        break;
+                    case "lt": 
+                        transform.Rotate(new Vector3(1, 0, 0) * currentCommand.val);
+                        progress = 100;
+                        break;
+                    case "rt":
+                        transform.Rotate(new Vector3(1, 0, 0) * currentCommand.val);
+                        progress = 100;
+                        break;  
+                    case "up":
+                        transform.Rotate(new Vector3(0, 1, 0) * currentCommand.val);
+                        progress = 100;
+                        break;
+                    case "dn":
+                        transform.Rotate(new Vector3(0, 1, 0) * -currentCommand.val);
+                        progress = 100;
+                        break;
+                }
+
+                Vector3 newPos = m_Rigidbody.position;
+
+                positionHistory.Add(newPos); 
+                line.SetVertexCount(positionHistory.Count);
+                line.SetPosition(positionHistory.Count-1, newPos);
                 commandQueue.Dequeue();
-                progress = 0.0f;
-            }
-            else 
-            {
-                progress += m_Speed;
+
+                // if (progress >= currentCommand.val)
+                // {
+                //     commandQueue.Dequeue();
+                //     progress = 0.0f;
+                // }
+                // else 
+                // {
+                //     progress += scale * Time.deltaTime;
+                // }
             }
         }
+
 
         // Keyboard controls (for testing)
         // // Go forwards
         if (Input.GetKey(KeyCode.UpArrow))
         {
             //Move the Rigidbody forwards constantly at speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.position += transform.forward * m_Speed * Time.deltaTime;
+            m_Rigidbody.position += transform.forward * scale * Time.deltaTime;
         }
 
         // Go backwards
         if (Input.GetKey(KeyCode.DownArrow))
         {
             //Move the Rigidbody backwards constantly at the speed you define (the blue arrow axis in Scene view)
-            m_Rigidbody.position -= transform.forward * m_Speed * Time.deltaTime;
+            m_Rigidbody.position -= transform.forward * scale * Time.deltaTime;
         }
 
         // Rotate Right
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //Rotate the sprite about the Y axis in the positive direction
-            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * scale, Space.World);
         }
 
         // Rotate Left
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             //Rotate the sprite about the Y axis in the negative direction
-            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * scale, Space.World);
         }
 
         // Rotate Up
@@ -111,14 +134,14 @@ public class Turtle : MonoBehaviour
             goHome();
 
             //Rotate the sprite about the X axis in the positive direction
-            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(1, 0, 0) * Time.deltaTime * scale, Space.World);
         }
 
         // Rotate Down
         if (Input.GetKey(KeyCode.W))
         {
             //Rotate the sprite about the Y axis in the negative direction
-            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * m_Speed, Space.World);
+            transform.Rotate(new Vector3(-1, 0, 0) * Time.deltaTime * scale, Space.World);
         }
 
 
